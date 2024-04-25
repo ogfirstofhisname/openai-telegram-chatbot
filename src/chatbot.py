5816c396ccf4206f514d981a369414da9007ea1c
ogfirstofhisname <133857929+ogfirstofhisname@users.noreply.github.com>
2024-04-25 12:05:18 +0300

    opened a new refactored script file

---
+++ src/chatbot.py
@@ -0,0 +1,9 @@
+
+
+### CHATBOT PARAMETERS - edit according to your use case ###
+############################################################
+
+
+debug_to_chat = False  # set to True to send debug messages to the chat, False to suppress them from the chat
+default_gpt_model = 'gpt-4-turbo'  # as of April 2024, the most capable LLM in the world + vision capabilities
+temperature = 0.5  # set to higher for more creative responses or lower for more deterministic output
